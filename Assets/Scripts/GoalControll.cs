24e99a84008412385f51571aadeefb27b65d991b
Tobiáš Huspenina <huspenina_tobias@oauh.cz>
2023-04-11 11:27:11 +0200

    mainscene

--- Assets/Scripts/GoalControll.cs
+++ Assets/Scripts/GoalControll.cs
@@ -2,70 +2,16 @@ using UnityEngine;
 
 public class ObjectCollision : MonoBehaviour
 {
-    public GameObject CheckBox1;
-    public GameObject CheckBox2;
-
-    private bool isCollidingWithCheckBox1;
-    private bool isCollidingWithCheckBox2;
-    private Vector3 defaultPosition;
+    
 
     void Start()
     {
-        // Uložení výchozí pozice objektu
-        defaultPosition = transform.position;
+        
     }
 
     void Update()
     {
-        // Reset the collision flags at the beginning of each frame
-        isCollidingWithCheckBox1 = false;
-        isCollidingWithCheckBox2 = false;
+        
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        // Check if the object collides with CheckBox1
-        if (other.gameObject == CheckBox1)
-        {
-            isCollidingWithCheckBox1 = true;
-        }
-
-        // Check if the object collides with CheckBox2
-        if (other.gameObject == CheckBox2)
-        {
-            isCollidingWithCheckBox2 = true;
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        // Check if the object stops colliding with CheckBox1
-        if (other.gameObject == CheckBox1)
-        {
-            isCollidingWithCheckBox1 = false;
-        }
-
-        // Check if the object stops colliding with CheckBox2
-        if (other.gameObject == CheckBox2)
-        {
-            isCollidingWithCheckBox2 = false;
-        }
-    }
-
-    void LateUpdate()
-    {
-        // If the object is not colliding with CheckBox1 and CheckBox2, do something
-        if (!isCollidingWithCheckBox1 && !isCollidingWithCheckBox2)
-        {
-            // Do something here
-            transform.position = defaultPosition;
-
-        }
-    }
-
-    void ReturnToDefaultPosition()
-    {
-        // Nastavení pozice objektu na výchozí pozici
-        transform.position = defaultPosition;
-    }
 }

517f69a1c82c1808aafd709046c94f3e14184a4d
0led <hubinak_oldrich@oauh.cz>
2023-05-02 11:28:08 +0200

    scenyy

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -9,6 +9,8 @@ public class Timer : MonoBehaviour
 {
     float currentTime;
     public float startingTime = 60f;
+    public TMP_Text score1;
+    public TMP_Text score2;
 
     [SerializeField] TMP_Text countdownText;
     void Start()
@@ -30,8 +32,21 @@ public class Timer : MonoBehaviour
         {
             currentTime = 0;
              countdownText.text = "0";
-             SceneManager.LoadScene(2);
-            // Your Code Here
+             int num1 = int.Parse(score1.text);
+        int num2 = int.Parse(score2.text);
+
+        if (num1 > num2)
+        {
+            SceneManager.LoadScene(3);
+        }
+        else if (num2 > num1)
+        {
+            SceneManager.LoadScene(4);
+        }
+        else
+        {
+            SceneManager.LoadScene(2);
+        }
         }
     }
 
